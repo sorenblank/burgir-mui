abfab9ff14b81b29d861c986e8f4dda1a4cb5ee2
sorenblank <afrozaman12@gmail.com>
2024-04-28 23:43:41 +0600

    Empty components

---
+++ app/components/Button.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Button from "@mui/material";
+
+
+const Button = ({className, children, orange}) => {
+    return (
+        <>
+          <Button>
+            Test
+          </Button>
+        </>
+    )
+  }
+  
+  export default Button;
