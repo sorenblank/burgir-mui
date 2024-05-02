587402dfa58421ac6ea6b8f62827c16246c235f5
sorenblank <afrozaman12@gmail.com>
2024-05-02 18:33:35 +0600

    Refactor Toggle component and update styles

--- app/components/Toggle.jsx
+++ app/components/Toggle.jsx
@@ -1,7 +1,52 @@
-const Toggle = () => {
-  return (
-    <div>Toggle</div>
-  )
-}
+import { Box } from '@mui/material';
 
-export default Toggle
+const Toggle = ({ clicked, onClick }) => {
+  return (
+    <Box
+      component="button"
+      sx={{
+        display: {xs: 'grid', md: 'none'},
+        gap: '4px',
+        cursor: 'pointer',
+        border: 'none',
+        background: 'none',
+        height: '100%',
+      }}
+      onClick={onClick}
+    >
+      <Box
+        sx={{
+          width: '10px',
+          height: '3px',
+          bgcolor: 'black',
+          borderRadius: '5px',
+          transition: '0.25s ease',
+          transform: clicked ? 'translate(1px, 3px) rotate(45deg)' : 'none',
+        }}
+      />
+      <Box
+        sx={{
+          width: '20px',
+          height: '3px',
+          bgcolor: 'black',
+          borderRadius: '5px',
+          transition: '0.25s ease',
+          transform: clicked ? 'rotate(-45deg)' : 'none',
+        }}
+      />
+      <Box
+        sx={{
+          width: '10px',
+          height: '3px',
+          bgcolor: 'black',
+          borderRadius: '5px',
+          ml: 'auto',
+          transition: '0.25s ease',
+          transform: clicked ? 'translate(-1px, -3px) rotate(45deg)' : 'none',
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Toggle;
